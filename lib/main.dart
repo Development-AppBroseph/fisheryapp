ec4e13143d7113b6b65ac59928de557a358653a8
daryn <akishevdarin@gmail.com>
2022-11-26 23:41:14 +0600

    new webview

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,24 @@ import 'dart:io';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:star_friends/web_view.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   name: 'FiheryApp',
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
-  // await FirebaseMessaging.instance.requestPermission(
-  //   alert: true,
-  //   announcement: false,
-  //   badge: true,
-  //   carPlay: false,
-  //   criticalAlert: false,
-  //   provisional: false,
-  //   sound: true,
-  // );
+  await Firebase.initializeApp(
+    name: 'FiheryApp',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
   // if (Platform.isAndroid) WebView.platform = AndroidWebView();
 
   runApp(
